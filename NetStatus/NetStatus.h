1d618cccbcef7c329b20cdaf078a80608b1ce37f
taind <nguyenductai.job@gmail.com>
2020-01-17 14:53:50 +0700

    Added framework source and podspec

---
+++ NetStatus/NetStatus.h
@@ -0,0 +1,19 @@
+//
+//  NetStatus.h
+//  NetStatus
+//
+//  Created by Tai Nguyen on 1/17/20.
+//  Copyright © 2020 Tai Nguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetStatus.
+FOUNDATION_EXPORT double NetStatusVersionNumber;
+
+//! Project version string for NetStatus.
+FOUNDATION_EXPORT const unsigned char NetStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetStatus/PublicHeader.h>
+
+
